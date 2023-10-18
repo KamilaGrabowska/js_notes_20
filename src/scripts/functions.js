8d57f58f9d375ece9c99da7f672dcc63895c4147
KAMILA <dudekkamila@icloud.com>
2023-10-18 10:17:04 +0200

    added quiz with object and function

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -9,38 +9,38 @@
 //Janusz to parametr
 
 
-// function greetings (name){
-//     return "Cześć, " + name;
-// }
-// console.log(greetings("Janusz"));
+ function greetings (name){
+ return "Cześć, " + name;
+}
+console.log(greetings("Janusz"));
 
-// function getDayName(day){
-//     if (day === 0) {
-//         return "niedziela";
-//     }
-//     if (day ===1){
-//         return "poniedziałek";
-//     }
-// }
-// console.log(getDayName(0));  niedziela
+function getDayName(day){
+ if (day === 0) {
+ return "niedziela";
+}
+if (day ===1){
+ return "poniedziałek";
+ }
+}
+console.log(getDayName(0));  niedziela
 
 
 // wypisz 6 licz z zakresu 0-49
-// let min =0;
-// let max = 49;
-// function drawDigit (quantity) {
-//     const temp = [];
-//
-//     while (temp.length < quantity ) {
-//         const digit  = Math.floor(Math.random() * 48 + 1);
-//         if(!temp.includes(digit)){
-//             temp.push(digit)
-//         }
-//
-//     }
-//    return temp;
-// }
-// console.log(drawDigit(8));
+let min =0;
+let max = 49;
+function drawDigit (quantity) {
+const temp = [];
+
+while (temp.length < quantity ) {
+const digit  = Math.floor(Math.random() * 48 + 1);
+if(!temp.includes(digit)){
+temp.push(digit)
+ }
+
+ }
+return temp;
+}
+console.log(drawDigit(8));
 
 
 
@@ -77,9 +77,8 @@ function arrayIntersection (arr1, arr2) {
 // 3.Zwróć losowy test zalezny od indeksu z tablicy
 
 
-function myLord (){
-    const sentense = ["Witaj mój Panie!", "Co jeszcze mogę dla Ciebie zrobić",
-    " Pięknie dzisiaj wygladasz!"]
+function myLord () {
+    const sentense = ["Witaj mój Panie!", "Co jeszcze mogę dla Ciebie zrobić", "Pięknie dzisiaj wygladasz!"];
     return sentense[Math.round(Math.round() * (sentense.length -1 ))];
 }
 
